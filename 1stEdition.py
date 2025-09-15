6d2de5aaa3e03e5d7ced4ee009465b73592a7aa8
VincentWilson <v1wilson@student.bridgew.edu>
2025-09-15 10:10:43 -0400

    Fully working version

--- 1stEdition.py
+++ 1stEdition.py
@@ -1,7 +1,7 @@
-numb_credits = input("How many credits are you currently taking?")
-numb_credits_prev_semester = input("How many credits did you take before the semester started?")
+numb_credits = int(input("How many credits are you currently taking?"))
+numb_credits_prev_semester = int(input("How many credits did you take before the semester started?"))
 total_credits_to_grad = 120
-credits_taken = numb_credits + numb_credits_prev_semester
-major = 'cybersecurity'
-credits_to_go = int(total_credits_to_grad - credits_taken)
-print("your major is", {major}, "and you have", {credits_to_go}, "to go untill you can graduate" )
+credits_taken = int(numb_credits + numb_credits_prev_semester)
+major = input("What is your major?")
+credits_to_go = total_credits_to_grad - credits_taken
+print(f"your major is {major} and you have {credits_to_go} to go until you can graduate")
